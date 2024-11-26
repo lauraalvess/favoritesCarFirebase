06438b51cbf8d0dd6539dc144946683f20b3b4fd
Laura_Beatriz_Alves <laura.alves@ifgoiano.edu.br>
2024-11-26 15:27:00 +0000

    send

--- lib/view/help_page.dart
+++ lib/view/help_page.dart
@@ -13,40 +13,14 @@ class HelpPage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     appState.preencherListaTodos();
 
-    //List<TodoModel> lista = appState.todosLista;
-
-    /** if (appState?.todosLista?.isEmpty ?? true) {
-      return Center(
-        child: Text('Não há registros.'),
-      );
-    }
-*/
-
-    /* return MaterialApp(
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        ),
-        debugShowCheckedModeBanner: false,
-        home: 
-        */
     return Scaffold(
         appBar: AppBar(
           title: Text('Lista de Todos'),
         ),
         body: LayoutBuilder(builder: (context, constraints) {
-          // if (constraints.maxWidth > 600) {
-          // Layout para telas maiores (ex: tablets, desktops)
           return Row(children: [
-            //Expanded(
-            //  flex: 1,
-            //  child: MyHomePage(),
-            //),
-            // VerticalDivider(width: 1),
             Expanded(
-              //   flex: 3,
-              child: //ListView.builder(
-                  ListView(
+              child: ListView(
                 children: [
                   for (TodoModel todo in appState.todosLista)
                     ListTile(
@@ -58,19 +32,8 @@ class HelpPage extends StatelessWidget {
                         //Navigator.of(context).pop();
                         Navigator.pushNamed(context, MyApp.TODOS_FORM,
                             arguments: todo);
-
-                        // Navigator.push(
-                        //  context,
-                        //   MaterialPageRoute(builder: (context) => TodoFormPage()),
-                        // );
                       },
                     ),
-                  //ElevatedButton(
-                  //onPressed: () {
-                  //Navigator.of(context).pop();
-                  //Navigator.pushNamed(context, MyApp.TODOS_LIST);
-                  // },
-                  // child: Text("Página Inicial"))
                 ],
               ),
             )
